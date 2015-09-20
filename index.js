b7aa78bcbe9f80c9ac7cbd379834fb37b43072ae
Simon Johansson <mail@simon-johansson.com>
2015-09-20 23:34:05 +0200

    Additions

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var debug = require('debug')('express:server');
 
+var api = require('littlebits-cloud-http')
+          .defaults({
+            access_token: '23ead4f1c1aa380cb1903f98a94e1c2b7dd1fda870997a387c376d17077ff120',
+            device_id: '00e04c1f0966',
+            duration_ms: -1
+          });
+
 var port = process.env.PORT || '3000';
 app.set('port', port);
 
@@ -13,19 +20,29 @@ server.listen(port, function () {
 
 io.on('connection', function (socket) {
 
+  api.device({device_id: '00e04c1f0966'}, function (err, data) {
+    // console.log(data);
+    socket.emit('is_online', data.is_connected);
+    // socket.emit('is_online', true);
+  });
+
   socket.on('start', function (data) {
-    console.log('start');
+    // console.log('start');
+    api.output({percent: '100'});
   });
 
   socket.on('stop', function (data) {
-    console.log('stop');
+    // console.log('stop');
+    api.output({percent: '0'});
   });
 });
 
-// var api = require('littlebits-cloud-http')
-//           .defaults({ access_token: '23ead4f1c1aa380cb1903f98a94e1c2b7dd1fda870997a387c376d17077ff120' })
-
 // var output = api.output.defaults({device_id: '00e04c1f0966', percent: 100, duration_ms: -1 })
+// api.output({device_id: '00e04c1f0966', percent: '0', duration_ms: -1 })
+// api.output({device_id: '00e04c1f0966', percent: '100', duration_ms: -1 })
 
+// api.output({percent: '100'});
+// api.output({percent: '0'});
 
-// api.output({device_id: '00e04c0362e9', percent: procent, duration_ms: -1 })
+// Ping devices för att se om man är på kontoret
+//
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,13 +1,29 @@
 
 var socket = io.connect('');
+var $doc = $(document);
+var $bell = $('.bell');
+var $mouth = $('.mouth');
 
-socket.on('news', function (data) {
-  console.log(data);
+socket.on('is_online', function (isOnline) {
+  var text, className;
+  if (isOnline) {
+    text = 'is online';
+    className = 'green';
+  } else {
+    text = 'is offline';
+    className = 'red';
+    $mouth.removeClass('hidden');
+    $bell.off('mousedown touchstart');
+  }
+  $('h2').html(text).addClass(className);
 });
 
-$('button').on('mousedown touchstart', function() {
-  $(document).on('mouseup touchend', function() {
-    $(document).off('mouseup touchend');
+$bell.on('mousedown touchstart', function() {
+  console.log('mousedown');
+  $bell.addClass('active');
+  $doc.on('mouseup touchend', function() {
+    $bell.removeClass('active');
+    $doc.off('mouseup touchend');
     socket.emit('stop');
   });
   socket.emit('start');
